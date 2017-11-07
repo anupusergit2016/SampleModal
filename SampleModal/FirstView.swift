e0a91c848b100a37b859af486da52ef5af67e271
Anup Samanta <anup.samanta@agnicient.com>
2017-11-07 17:40:13 +0530

    add README

---
+++ SampleModal/FirstView.swift
@@ -0,0 +1,21 @@
+//
+//  FirstView.swift
+//  SampleModal
+//
+//  Created by GiriAdmin on 27/09/17.
+//  Copyright © 2017 AgniAdmin. All rights reserved.
+//
+
+import UIKit
+
+class FirstView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SampleModal/SecondView.swift
@@ -0,0 +1,21 @@
+//
+//  SecondView.swift
+//  SampleModal
+//
+//  Created by GiriAdmin on 28/09/17.
+//  Copyright © 2017 AgniAdmin. All rights reserved.
+//
+
+import UIKit
+
+class SecondView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
